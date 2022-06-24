208c5d8271f49076dff93beda645f29c4cc8b56b
Serihusna <2020862706@student.uitm.edu.my>
2022-06-24 14:13:18 +0800

    commit

--- next_page.php
+++ next_page.php
@@ -1,6 +1,11 @@
 <?php
 session_start ();
 echo "<html>";
+$sid =session_id();
+echo "Session ID returned by session_id():".$sid."\n";
+$sid=SID;
+echo "Session ID returned by SID :".$sid."\n";
+
 $myLogin =$_SESSION ["myLogin"];
 echo " Value of myLogin has been retrieved: ".$myLogin."\n";
 
